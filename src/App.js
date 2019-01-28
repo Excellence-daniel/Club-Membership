680ac1599b13e5c8c49fe3081706747af17c49ed
Shalom <oyeniranexcellenced@gmail.com>
2019-01-28 22:29:34 +0100

    Begin

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import fire from './config/fire'
+// import Home from './pages/homePage'
+// import Login from './pages/login'
+import Links from './links'
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      user : {}, 
+      email : '', 
+      password : ''
+    }
+  }
+
+  componentDidMount(){
+    this.authListener()
+  }
+
+  authListener(){
+    fire.auth().onAuthStateChanged((user) => {
+      if(user){
+        this.setState({user})
+      } else {
+        this.setState({user : null})
+      }
+    })
+  }
+
+  login = () => {
+    fire.auth().signInWithEmailAndPassword(this.setState.email, this.state.password).then((u)=> {
+    }).catch((error)=>{
+      console.log(error)
+    })
+  }
+ 
+  render(){
+    return (
+        <div> 
+            <Links/>
+        </div>
+    )
+  }
+}
+  
+
+export default App;
