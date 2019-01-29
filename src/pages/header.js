7d10e7eece6f6cd03568b26daf797830d2eb0aa0
Shalom <oyeniranexcellenced@gmail.com>
2019-01-29 16:20:43 +0100

    Styled header.js

--- src/pages/header.js
+++ src/pages/header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link, Redirect} from 'react-router-dom';
 import '../index.css'
 
@@ -7,57 +7,93 @@ const liStyle = {
     display : 'block'
 }
 
-function logOut(){
-    fire.auth().signOut()
-    .then(function(){
-        localStorage.setItem("LOGIN", false)
-        console.log("HEY")
-    })
-    return <Redirect to = '/'/>
-}
+class Header extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            status : false,
+            redirect : false
+        }
+    }
+    componentDidMount(){
+        //console.log("HERWE", this.props.isloggedIn)
+        var status = this.props.isloggedIn
+        this.setState({status})
+        var getLOGIN = localStorage.getItem("LOGIN")
+        if (!getLOGIN){
+            localStorage.setItem("LOGIN", false)
+        }
+    }
 
-function Header (){
-   const status = localStorage.getItem("LOGIN")
-   console.log(status)
-        if(status !== true){
+    logOut = () => {
+            fire.auth().signOut()
+            .then(() => {
+                localStorage.setItem("LOGIN", false);
+            })
+        }
+    render(){   
+        if (this.state.redirect === true ){
+            return <Redirect to = '/'/>
+        }     
+        console.log("STATE STATUS", this.state.status, "LS STATUS", localStorage.getItem("LOGIN"))
+        if(this.state.status === false){
             return (
                 <div className = "col-md-12">
-                    <div className = "col-md-4"> 
-                        <h2> CLUB MEMBERSHIP APP </h2>
-                    </div>
-                    <div className = "col-md-5"></div>
-                    <div className = "col-md-3 mt-2"> 
-                        <nav className ="nav nav-pills nav-justified">
-                             <Link to = "/login" className = "nav-link" style = {{color: '#fff',backgroundColor: "#337ab7"}}>  LOGIN  </Link>
-                                &nbsp; &nbsp;
-                             <Link to = "/signup" className = "nav-link" style = {{color: '#fff',backgroundColor: "#337ab7"}}> SIGN UP </Link>
-                        </nav>
+                    <div className = "col-md-12" > 
+                        <center><h1> CLUB MEMBERSHIP APP </h1></center>
+                        <div className = "col-md-5"></div>
+                        <div className = "col-md-2 mt-3">
+                            <center>
+                                <nav className ="nav nav-pills nav-justified">
+                                    <Link to = "/login" className = "nav-link" style = {{color: '#fff',backgroundColor: "#337ab7"}}>  LOGIN  </Link>
+                                        &nbsp; &nbsp;
+                                    <Link to = "/signup" className = "nav-link" style = {{color: '#fff',backgroundColor: "#337ab7"}}> SIGN UP </Link>
+                                </nav>
+                            </center>
+                        </div>
+                        <div className = "col-md-5"> 
+                        </div>
                     </div>
                 </div>
             )
         } else {
            return (
             <div className = "col-md-12">
-                <div className = "col-md-4"> 
-                    <h2> CLUB MEMBERSHIP APP </h2>
-                </div>
-                <div className = "col-md-5"></div>
-                <div className = "col-md-3 mt-2">
-                    <ul>
-                        <Link to = "./profile/viewProfile" style = {{textDecoration : 'none', cursor : 'pointer'}}> <li> PROFILE </li> </Link>
-                        <li className ="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style = {{textDecoration : 'none', cursor : 'pointer'}}> CLUBS </li>
-                                    <ul className ="dropdown-menu" id="drop-menu" >
-                                        <Link to = "/club/createClub" className = "nav-link" style = {{color: '#fff',backgroundColor: "#337ab7"}}> Create Clubs </Link>
-                                        <Link to = "/club/addMembers"><li style = {liStyle}>Add Members to Club (via Email) </li></Link>  
-                                        <Link to = "/club/viewClubs"><li style = {liStyle}> View Created Clubs </li></Link>
-                                    </ul>
+                    <div className = "col-md-12" > 
+                        <center><h1> CLUB MEMBERSHIP APP </h1></center>
+                        <div className = "col-md-4"></div>
+                        <div className = "col-md-4 mt-3">
+                            <center>
+                            <ul>
+                                <Link to = "/profile/viewProfile" style = {{textDecoration : 'none', cursor : 'pointer', color: 'black'}}> <li> PROFILE </li> </Link>
+                                <li className ="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style = {{textDecoration : 'none', color : 'black', cursor : 'pointer'}}>
+                                             CLUBS 
+                                </li>
+                                            <ul className ="dropdown-menu" id="drop-menu" >
+                                                <Link to = "/club/createClub" style = {{textDecoration : 'none', cursor:'pointer'}}> Create Clubs </Link>
+                                                <Link to = "/club/addMembers" style = {{textDecoration : 'none', cursor:'pointer'}}>>
+                                                        <li style = {liStyle}>
+                                                            Add Members to Club (via Email) 
+                                                        </li>
+                                                </Link>  
+                                                <Link to = "/club/viewClubs" style = {{textDecoration : 'none', cursor:'pointer'}}>>
+                                                        <li style = {liStyle}> 
+                                                            View Created Clubs 
+                                                        </li>
+                                                </Link>
+                                            </ul>
+                                <li> <button className = "btn btn-danger" onClick = {this.logOut}> LOG OUT </button> </li>
+                            </ul>
+                            </center>
+                        </div>
+                        <div className = "col-md-4"> 
+                            
+                        </div>
+                    </div>
 
-                        <li> <button className = "btn btn-danger" onClick = {logOut}> LOG OUT </button> </li>
-                    </ul>
-                </div>
             </div>
            )
         }     
+    }
 }
-
 export default Header;
