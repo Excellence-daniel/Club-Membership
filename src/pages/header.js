d3d3cc838bc4d2736be6d6b5ca7b2e5718125f8b
Shalom <oyeniranexcellenced@gmail.com>
2019-01-29 06:57:26 +0100

    On login, the menu option changes and the page redirects

--- src/pages/header.js
+++ src/pages/header.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
 const liStyle = {
     display : 'block'
@@ -6,64 +6,48 @@ const liStyle = {
 
 
 
-class Header extends Component{
-    constructor(props){
-        super(props);
-        this.state ={
-            loginStatus : true
-        }
-    }
-
-    loginStatus = () => {
-        if(this.state.loginStatus === false){
+function Header (props){
+        if(props.loginStatus === false){
             return (
-                <div> 
-                    <ul>
-                            <div>
-                            <Link to = "./login"> <li> LOGIN </li> </Link>
-                            <Link to = "./signup"> <li> SIGN UP </li> </Link>
-                           
-                            </div>
-                    </ul>
-                </div>
-            )
-        } else {
-           return (
-            <div>
-                <ul>
-                    <Link to = "./profile/viewProfile"> <li> PROFILE </li> </Link>
-                    <li class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> CLUBS </li>
-								  <ul class="dropdown-menu" id="drop-menu" >
-                                    <Link to = "./club/createClub"><li style = {liStyle}> Create Clubs </li></Link>
-                                    <Link to = "./club/addMembers"><li style = {liStyle}>Add Members to Club (via Email) </li></Link>  
-                                    <Link to = "./club/viewClubs"><li style = {liStyle}> View Created Clubs </li></Link>
-                                  </ul>
-
-                    <li> <button class = "btn btn-danger"> LOG OUT </button> </li>
-                </ul>
-            </div>
-           )
-        }
-    }
-
-    render(){
-        return (
-            <div>
                 <div className = "col-md-12">
                     <div className = "col-md-4"> 
                         <h2> CLUB MEMBERSHIP APP </h2>
                     </div>
                     <div className = "col-md-5"></div>
-                    <div className = "col-md-3 mt-2">
-                        {this.loginStatus()}
+                    <div className = "col-md-3 mt-2"> 
+                        <ul className = "nav nav-pills nav-fill">
+                                <Link to = "./login"> <li className = "nav-item"> LOGIN </li> </Link>
+                                <Link to = "./signup"> <li className = "nav-item"> SIGN UP </li> </Link>
+                        </ul>
                     </div>
                 </div>
-               
-                
+            )
+        } else {
+           return (
+            <div className = "col-md-12">
+                <div className = "col-md-4"> 
+                    <h2> CLUB MEMBERSHIP APP </h2>
+                </div>
+                <div className = "col-md-5"></div>
+                <div className = "col-md-3 mt-2">
+                    <ul>
+                        <Link to = "./profile/viewProfile"> <li> PROFILE </li> </Link>
+                        <li className ="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> CLUBS </li>
+                                    <ul className ="dropdown-menu" id="drop-menu" >
+                                        <Link to = "./club/createClub"><li style = {liStyle}> Create Clubs </li></Link>
+                                        <Link to = "./club/addMembers"><li style = {liStyle}>Add Members to Club (via Email) </li></Link>  
+                                        <Link to = "./club/viewClubs"><li style = {liStyle}> View Created Clubs </li></Link>
+                                    </ul>
 
-            </div> 
-        )
-    }
+                        <li> <button className = "btn btn-danger" onClick = {props.logOut}> LOG OUT </button> </li>
+                    </ul>
+                </div>
+            </div>
+           )
+        }
+    
+
+       
 }
 
 export default Header;
