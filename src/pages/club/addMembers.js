aa400b268073fa648f4c12b18c71d4ca9d5bd00c
Shalom <oyeniranexcellenced@gmail.com>
2019-01-29 01:04:12 +0100

    Here is where we invite users using emails

---
+++ src/pages/club/addMembers.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+
+import Header from '../header';
+import {db} from '../config/fire'
+
+class AddMembers extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            clubProfile : {},
+            id : ''
+        }
+    }
+
+    componentDidMount(){
+        var club = localStorage.getItem("Clubs");
+        db.collection('Clubs').where("ClubName", "==", club).get()
+        .then((snapshot) => {
+            snapshot.forEach((doc,id) =>{
+                console.log(doc.data())
+                this.setState({clubProfile : doc.data(), id : doc.id })
+                // alert(doc.id)
+            })
+        })
+        console.log(club)
+    }
+
+    render(){
+        
+       return (
+            <div class = "col-md-12"> 
+                <div class = "col-md-12">
+                    <Header/>
+                </div>
+                <div class = "col-md-12">
+                    <div className = "col-md-3 mx-auto"> 
+                        <h2> CLUB </h2>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default AddMembers;
