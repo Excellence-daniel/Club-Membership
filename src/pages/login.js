964aecf58d52794cb52726380be0d0e1921f6e16
Shalom <oyeniranexcellenced@gmail.com>
2019-02-01 00:43:21 +0100

    Added a loader on click of the login button

--- src/pages/login.js
+++ src/pages/login.js
@@ -4,60 +4,53 @@ import {Redirect} from 'react-router-dom'
 import {fire,db} from './config/fire'
 import Header from './header'
 
-
 class LoginUser extends Component {
     constructor(props){
         super(props);
         this.state = {
             email : '',
             password : '', 
-            redirect : false
+            redirect : false, 
+            displayLoader : 'none'
         }
     }
 
     handleEmailInput = (e) => {
             this.setState({email : e.target.value})
-        console.log(this.state.email)
     } 
 
     handlePasswordInput = (e) => {        
             this.setState({password : e.target.value})
-        console.log(this.state.password)
-    } 
-
-    calllogin=(email, password)=> {
-        fire.auth().signInWithEmailAndPassword(email, password).then((u)=> {
-            console.log(u , "SUCCESS");
-            alert("You are logged in");
-            this.setState({redirect : true})            
-        }).catch((error)=>{
-          console.log(error)
-          alert("Wrong Email or Password")
-        })
     } 
 
     login = async (e) => {
         e.preventDefault();
+        const loader = document.getElementById('loader').style
+        loader.display = 'block' //start loader
         const {email, password} =  this.state
         if (email === '' || password === ''){
             alert("Fill in all fields")
+            loader.display = 'none'
         }else {
-            const getUserCollection =  await db.collection('Users').where("Email", "==", email).get()
-            const getUser = await getUserCollection.docs[0]._document.proto.fields;
-            const getUserEmailVer = await getUser.EmailVerified.booleanValue //to get if user is verified
-            
-            if (getUserEmailVer === false){
-                alert("Verify your email before you login")
-                this.setState({redirect : true})
-            } else {
+            const getUserCollection =  await db.collection('Users').where("Email", "==", email).get() //get users from data with the email specified
+            let getUserEmailVer; 
+            getUserCollection.forEach((snapshot)=>{
+            getUserEmailVer = snapshot.data().EmailVerified  //status of email verification
+        })
+        loader.display = 'none' //stop the loader
+
+        if (getUserEmailVer === false){
+            alert("Verify your email before you login")
+            this.setState({redirect : true})
+        } else {
                 fire.auth().signInWithEmailAndPassword(email, password).then((u)=> {
-                    console.log(u , "SUCCESS");
-                    alert("You are logged in");
-                    this.setState({redirect : true})            
-                }).catch((error)=>{
-                    console.log(error)
-                    alert("Wrong Email or Password")
-                })
+                console.log(u , "SUCCESS");
+                alert("You are logged in");
+                this.setState({redirect : true})            
+            }).catch((error)=>{
+                console.log(error)
+                alert("Wrong Email or Password. Try Again!")
+            })
             }
         }
     }      
@@ -65,8 +58,9 @@ class LoginUser extends Component {
     render(){
         const redirect = this.state.redirect
         if (redirect){
-            return <Redirect to = '/'/>
+            return <Redirect to = '/'/> //redirect to the home page
         }
+
         return (
             <div className = "col-md-12">
                 <div className = "col-md-12">
@@ -84,7 +78,8 @@ class LoginUser extends Component {
                             <input type = "password" onChange = {this.handlePasswordInput} className = "form-control"/>
                         </p>
                         <button className = "btn btn-primary btn-block" onClick = {this.login}> LOGIN </button>
-                    </form>       
+                    </form>
+                    <div className = ""><center><img src = "img/loader.gif" alt = "loader" style = {{display : 'none', width: '30%'}} id = "loader"/></center></div>       
                 </div>
                 <div className = "col-md-5"></div>
             </div>
