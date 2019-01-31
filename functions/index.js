3c16e44687216ae90589ba5d065f03c01a45bc25
Shalom <oyeniranexcellenced@gmail.com>
2019-01-31 11:22:00 +0100

    Firebase functions file created

---
+++ functions/index.js
@@ -0,0 +1,97 @@
+const functions = require('firebase-functions');
+
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+const SENDGRID_API_KEY = functions.config().sendgrid.key;
+
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(SENDGRID_API_KEY);
+
+exports.firestoreValidateNewUsers = functions.firestore
+    .document('Users/{UsersId}')
+    .onCreate((event) => new Promise((resolve,reject) => { 
+        const userData = event.data();
+        console.log("user data", userData)
+                    const user = userData
+                    const url = "localhost:3000/verifyEmail"
+                if (user){
+                    const msg = {
+                        to: user.Email,
+                        from: 'ClubMembershipapp@gmail.com',
+                        subject:  'Verify Email',
+                        // text: `Hey ${toName}. You have a new follower!!! `,
+                        html: `
+                            <strong> Hey ${user.Name}. Click on the link below to verify your email. </strong>
+                                <br/><br/><br/>
+                            <center>
+                                <a href = ${url}> hjhkcnlshdjhdjsscuhdsu37ew890ruwe8eujnshc7832995294kjschdsxjc </a>
+                            </center>
+                        `,
+                }
+            sgMail.send(msg)
+            .then(()=>console.log('Email.sent'))
+             .catch((e) => {
+                console.log(e)
+             })              
+}}))
+
+exports.firestoreInviteEmail = functions.firestore
+    .document('Clubs/{ClubsId}')
+    .onUpdate((change, context) => new Promise((resolve,reject) => { 
+        const ClubInviteData = change.after.data();
+        console.log("Change After ", change.after.data())
+        console.log("Before Data", change.before.data())
+        // console.log("ClubInviteData", ClubInviteData)
+//                     const user = userData
+//                     const 
+//                     const url = "localhost:3000/verifyEmail"
+//                 if (user){
+//                     const msg = {
+//                         to: user.Email,
+//                         from: 'ClubMembershipapp@gmail.com',
+//                         subject:  'Verify Email',
+//                         // text: `Hey ${toName}. You have a new follower!!! `,
+//                         html: `
+//                             <strong> Hey ${user.Name}. Click on the link below to verify your email. </strong>
+//                                 <br/><br/><br/>
+//                             <center>
+//                                 <a href = ${url}> hjhkcnlshdjhdjsscuhdsu37ew890ruwe8eujnshc7832995294kjschdsxjc </a>
+//                             </center>
+//                         `,
+//                 }
+//             //sgMail.send(msg)
+//             .then(()=>console.log('Email.sent'))
+//              .catch((e) => {
+//                 console.log(e)
+//              })              
+// }
+}))
+
+// exports.firestoreInviteEmail = functions.firestore
+//     .document('Clubs/{ClubsId}/Invites')
+//     .onCreate((event) => new Promise((resolve,reject) => { 
+//         const userData = event.data();
+//         console.log("user data", userData)
+//                     const user = userData
+//                     const url = "localhost:3000/verifyEmail"
+//                 if (user){
+//                     const msg = {
+//                         to: user.Email,
+//                         from: 'ClubMembershipapp@gmail.com',
+//                         subject:  'Verify Email',
+//                         // text: `Hey ${toName}. You have a new follower!!! `,
+//                         html: `
+//                             <strong> Hey ${user.Name}. Click on the link below to verify your email. </strong>
+//                                 <br/><br/><br/>
+//                             <center>
+//                                 <a href = ${url}> hjhkcnlshdjhdjsscuhdsu37ew890ruwe8eujnshc7832995294kjschdsxjc </a>
+//                             </center>
+//                         `,
+//                 }
+//             sgMail.send(msg)
+//             .then(()=>console.log('Email.sent'))
+//             .catch((e) => {
+//                 console.log(e)
+//             })              
+//     }}))
