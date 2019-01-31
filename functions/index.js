54cc6cac31898ffd8a8a0fd6e582056bf7c0eff1
Shalom <oyeniranexcellenced@gmail.com>
2019-01-31 12:40:10 +0100

    Added a function to sent invites to users

--- functions/index.js
+++ functions/index.js
@@ -37,37 +37,35 @@ exports.firestoreValidateNewUsers = functions.firestore
 }}))
 
 exports.firestoreInviteEmail = functions.firestore
-    .document('Clubs/{ClubsId}/Members')
+    .document('Clubs/{ClubsId}')
     .onUpdate((change, context) => new Promise((resolve,reject) => { 
-        const ClubInviteData = change.after.data();
-        console.log("Change After ", change.after.data())
-        console.log("Before Data", change.before.data())
-        console.log("NEW DATA", ClubInviteData.Members)
-        // console.log("ClubInviteData", ClubInviteData)
-//                     const user = userData
-//                     const 
-//                     const url = "localhost:3000/verifyEmail"
-//                 if (user){
-//                     const msg = {
-//                         to: user.Email,
-//                         from: 'ClubMembershipapp@gmail.com',
-//                         subject:  'Verify Email',
-//                         // text: `Hey ${toName}. You have a new follower!!! `,
-//                         html: `
-//                             <strong> Hey ${user.Name}. Click on the link below to verify your email. </strong>
-//                                 <br/><br/><br/>
-//                             <center>
-//                                 <a href = ${url}> hjhkcnlshdjhdjsscuhdsu37ew890ruwe8eujnshc7832995294kjschdsxjc </a>
-//                             </center>
-//                         `,
-//                 }
-//             //sgMail.send(msg)
-//             .then(()=>console.log('Email.sent'))
-//              .catch((e) => {
-//                 console.log(e)
-//              })              
-// }
-}))
+        const newData = change.after.data()
+        const adminName = newData.AdminName
+        const clubName = newData.ClubName
+        const allMembers = newData.Members;
+        const currentMemberID = allMembers.length - 1 
+        const currentMemberEmail = allMembers[currentMemberID].name
+        console.log("NEW EMAIL", currentMemberEmail)
+        const url = "localhost:3000/club/joinClub.js"
+                if (user){
+                    const msg = {
+                        to: currentMemberEmail,
+                        from: 'ClubMembershipapp@gmail.com',
+                        subject:  'Verify Email',
+                        html: `
+                            <strong> Hey. An invite was sent to you by ${adminName} to join a club on our app. Click on the link below to view.  </strong>
+                                <br> <br>
+                            <center>
+                                <a href = ${url}><button> View Invite Message </button></a>
+                            </center>
+                        `,
+                }
+                sgMail.send(msg)
+            .then(()=>console.log('Email.sent'))
+             .catch((e) => {
+                console.log(e)
+             })              
+}}))
 
 // exports.firestoreInviteEmail = functions.firestore
 //     .document('Clubs/{ClubsId}/Invites')
