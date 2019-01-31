b71e75d31cdd97761b82016b94fa77cfd5f4762e
Shalom <oyeniranexcellenced@gmail.com>
2019-01-31 11:12:30 +0100

    Added routes to joinClub and verifyEMail for new users

--- src/pages/router.js
+++ src/pages/router.js
@@ -9,6 +9,9 @@ import ViewClubs from './club/viewClubs'
 import AddMembers from './club/addMembers'
 import CreateClub from './club/createClub'
 import EditClub from './club/editClub'
+import JoinClub from './club.joinClub'
+
+import VerifyEmail from './verifyEmail'
 
 import ViewProfile from './profile/viewProfile'
 
@@ -24,11 +27,13 @@ function Routerr (){
                 <Route exact path = "/" component = {LandingPage}/>
                 <Route exact path = "/login" component = {LoginUser}/>
                 <Route exact path = "/signup" component = {SignUpUser}/>
+                <Route exact path = "/verifyEmail" component = {VerifyEmail}/>
                 <Route exact path = "/club/viewClubs" component = {ViewClubs}/>
                 <Route exact path = "/club/addMembers" component = {AddMembers}/>
                 <Route exact path = "/club/createClub" component = {CreateClub}/>
                 <Route exact path = "/profile/viewProfile" component = {ViewProfile}/>
                 <Route exact path = "/clubs/editClub" render ={(props) => <EditClub {...props}/>}/>
+                <Route exact path = "/clubs/joinClub" component = {JoinClub}/>
             </div>  
         </Router>
     )
