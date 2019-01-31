7e86df974064e8506387f11b30b856ee760aa52a
Shalom <oyeniranexcellenced@gmail.com>
2019-02-01 00:42:43 +0100

    Can sent invites to emails now

--- src/pages/club/addMembers.js
+++ src/pages/club/addMembers.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Redirect} from 'react-router-dom'
 
 import Header from '../header';
 import {fire,db} from '../config/fire'
@@ -11,15 +12,18 @@ class AddMembers extends Component {
             clubs : [], 
             invitedEmail : '',
             inviteToClub : '', 
+            redirect : false
         }
     }
 
     componentDidMount= async()=>{
+        const loader = document.getElementById('loader').style 
+        loader.display = 'block'
         var user = fire.auth().currentUser
         if (user){
             console.log("User ID: ", user.uid)
             console.log("USER EAIL", user.email)
-            const getClubs = await db.collection('Clubs').where("Email", "==", user.email).get()
+            const getClubs = await db.collection('Clubs').where("AdminEmail", "==", user.email).get()
             let clubnames = []
             getClubs.forEach((snapshot)=>{
                 var clubname = snapshot.data().ClubName
@@ -29,6 +33,7 @@ class AddMembers extends Component {
         } else {
             console.log("No user")
         }
+        loader.display = 'none'
         console.log("STATE CLUBS ", this.state.clubs)
     }
 
@@ -45,8 +50,11 @@ class AddMembers extends Component {
     }
 
     inviteMembers = async () => {
+        const loader = document.getElementById('loader').style 
+        loader.display = 'block'
         if (this.state.invitedEmail === '' || this.state.inviteToClub === ''){
             alert("Fill in the fileds")
+            loader.display = 'none'
         } else {
             const invitee = {"name":this.state.invitedEmail, "accepted" : false}
             const getClubDetails = await db.collection('Clubs').doc(this.state.clubId).get() //gets club details from firebase
@@ -55,30 +63,45 @@ class AddMembers extends Component {
             await db.collection('Clubs').doc(this.state.clubId).update({
                 Members : getMembers
             })
-
-            console.log(invitee, "MEMBERS", getMembers)
+            .then(()=> {
+                console.log("Invite Sent!")
+                alert("Invite Sent!")
+            })
+            loader.display = 'none'
+            this.setState({redirect : true})
         }
     }
 
     render(){
+        if(this.state.redirect === true){
+            return <Redirect to = "/"/>
+        }
        return (
             <div class = "col-md-12"> 
                 <div class = "col-md-12">
                     <Header/>
                 </div>
                 <div class = "col-md-12">
-                    <div className = "col-md-3 mx-auto"> 
-                        <h2> Add Members</h2>
-                            Invite : 
-                        <input type = "email" onChange = {this.emailToInvite} class = "form-control"/>
-                            to 
-                        <select class = "form-control" onChange = {this.pickClub}>
-                            {this.state.clubs.map(club => (
-                                <option key ={club} value ={club}> {club} </option>
-                            ))}
-                        </select>
-                        <button class = "btn btn-danger" onClick = {this.inviteMembers}> INVITE </button>
+                    <div className = "col-md-4"></div>
+                    <div className = "col-md-4 mx-auto" style = {{fontSize : '18px'}}>
+                        <center> 
+                            <h2> Add Members</h2>
+                            <br/>
+                                Invite : 
+                            <input type = "email" onChange = {this.emailToInvite} class = "form-control"/>
+                                to 
+                            <select class = "form-control" onChange = {this.pickClub}>
+                                <option value = ""> SELECT A CLUB </option>
+                                {this.state.clubs.map(club => (
+                                    <option key ={club} value ={club}> {club} </option>
+                                ))}
+                            </select>
+                            <br/>
+                            <div><button class = "btn btn-danger btn-block" onClick = {this.inviteMembers}> INVITE </button></div>
+                        </center>
+                        <div className = ""><center><img src = "../img/loader.gif" alt = "loader" style = {{display : 'none', width: '10%'}} id = "loader"/></center></div>
                     </div>
+                    <div className = "col-md-4"></div>
                 </div>
             </div>
         )
